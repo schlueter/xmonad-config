e31e88d26d35ad31aacf935ad9df44d1d83ca460
Schlueter <bs@schlueter.blue>
2017-04-22 15:36:18 -0400

    Cleanup; fix mediacontrol interaction

--- xmonad.hs
+++ xmonad.hs
@@ -22,9 +22,10 @@ import qualified XMonad.StackSet as W
 myScreensaver = "/usr/bin/gnome-screensaver-command --lock"
 myScreenshot = "scrot '%F-%T_$wx$h.png' -e 'mv $f ~/Pictures/Screenshots'"
 myFocusedScreenshot = myScreenshot ++ " -u"
+mySelectionScreenshot = myScreenshot ++ " -s"
 myLauncher = "$(yeganesh -x -- -fn '-*-terminus-*-r-normal-*-*-120-*-*-*-*-iso8859-*')"
 myMediaControl = "media-control" ++ " "
-myNotify = "notify-send -h string:x-canonical-private-synchronous:anything"
+myNotify = "notify-send -h string:x-canonical-private-synchronous:anything "
 myBrowser = "firefox"
 myPrivateBrowser = "firefox --private-window"
 clipboardManager = "clipmenu"
@@ -37,23 +38,24 @@ delkeys XConfig {XMonad.modMask = modMask} =
 
 inskeys :: XConfig l -> [((KeyMask, KeySym), X ())]
 inskeys conf@(XConfig {XMonad.modMask = modMask}) =
-  [ ((modMask .|. controlMask, xK_l),    spawn (myNotify ++ " Locking;" ++ myScreensaver))
-  , ((modMask, xK_p),                    spawn myLauncher)
-  , ((modMask, xK_s),                    spawn myFocusedScreenshot)
-  , ((modMask .|. shiftMask, xK_s),      spawn myScreenshot)
-  , ((modMask, xK_backslash),            spawn clipboardManager)
-  , ((modMask, xK_b),                    spawn myBrowser)
-  , ((modMask .|. shiftMask, xK_b),      spawn myPrivateBrowser)
-  , ((modMask, xK_i),                    spawn toggleInput)
-  , ((0, xF86XK_AudioPrev),              spawn (myMediaControl ++ "previous"))
-  , ((0, xF86XK_AudioPlay),              spawn (myMediaControl ++ "play-pause"))
-  , ((0, xF86XK_AudioNext),              spawn (myMediaControl ++ "next"))
-  , ((0, xF86XK_AudioMute), spawn (myMediaControl ++ "toggle-mute"))
-  , ((0, xF86XK_AudioLowerVolume), spawn (myMediaControl ++ "-1%"))
-  , ((0, xF86XK_AudioRaiseVolume), spawn (myMediaControl ++ "+1%"))
+  [ ((modMask .|. controlMask, xK_l), spawn (myNotify ++ "Locking;" ++ myScreensaver))
+  , ((modMask, xK_p), spawn myLauncher)
+  , ((modMask, xK_s), spawn (myNotify ++ "'Taking selection screenshot';" ++ mySelectionScreenshot))
+  , ((modMask .|. controlMask, xK_s), spawn (myNotify ++ "'Taking focused screenshot';" ++ myFocusedScreenshot))
+  , ((modMask .|. shiftMask, xK_s), spawn (myNotify ++ "'Taking screenshot';" ++ myScreenshot))
+  , ((modMask, xK_backslash), spawn clipboardManager)
+  , ((modMask, xK_b), spawn myBrowser)
+  , ((modMask .|. shiftMask, xK_b), spawn myPrivateBrowser)
+  , ((modMask, xK_i), spawn toggleInput)
   , ((0, xF86XK_PowerOff), spawn powerButton)
-  , ((0 .|. shiftMask, xF86XK_AudioLowerVolume), spawn (myMediaControl ++ "-10%"))
-  , ((0 .|. shiftMask, xF86XK_AudioRaiseVolume), spawn (myMediaControl ++ "+10%"))
+  , ((0, xF86XK_AudioPrev), spawn (myMediaControl ++ "previous"))
+  , ((0, xF86XK_AudioPlay), spawn (myMediaControl ++ "play-pause"))
+  , ((0, xF86XK_AudioNext), spawn (myMediaControl ++ "next"))
+  , ((0, xF86XK_AudioMute), spawn (myMediaControl ++ "toggle-mute"))
+  , ((0, xF86XK_AudioLowerVolume), spawn (myMediaControl ++ "-1"))
+  , ((0, xF86XK_AudioRaiseVolume), spawn (myMediaControl ++ "+1"))
+  , ((shiftMask, xF86XK_AudioLowerVolume), spawn (myMediaControl ++ "-"))
+  , ((shiftMask, xF86XK_AudioRaiseVolume), spawn (myMediaControl ++ "+"))
   ]
   ++
   -- These have to be added back because they go away for some reason
@@ -61,25 +63,27 @@ inskeys conf@(XConfig {XMonad.modMask = modMask}) =
     | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
-main = do
-  xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
-  xmonad $ defaultConfig
-    { focusFollowsMouse = False
-    , keys = customKeys delkeys inskeys
-    , layoutHook = smartBorders $
-        avoidStruts
-        ( Tall 1 (3/100) (1/2)
-          ||| tabbed shrinkText defaultTheme { activeColor = "#000000" }
-        ) ||| noBorders (fullscreenFull Full)
-    , logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn xmproc }
-    , manageHook = manageDocks <+> composeAll
+myLayout = smartBorders $ avoidStruts (
+        Tall 1 (3/100) (1/2)
+        ||| tabbed shrinkText defaultTheme { activeColor = "#000000" }
+    ) ||| noBorders (fullscreenFull Full)
+
+myManageHook = manageDocks <+> composeAll
         [ className =? "Dialog" --> doFloat
         , className =? "Gimp" --> doFloat
         , className =? "Xmessage" --> doFloat
         , title =? "Password Required" --> doFloat
         , resource =? "desktop_window" --> doIgnore
-        , isFullscreen --> doFullFloat
-        ]
+        , isFullscreen --> doFullFloat ]
+
+main = do
+  xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
+  xmonad $ defaultConfig
+    { focusFollowsMouse = False
+    , keys = customKeys delkeys inskeys
+    , layoutHook = myLayout
+    , logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn xmproc }
+    , manageHook = myManageHook
     , modMask = mod4Mask
     , terminal = "/usr/bin/terminology"
     }
