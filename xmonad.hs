08e45b96e35908764d053ee797a3e8259a0cf9d2
Schlueter <bs@schlueter.blue>
2017-06-30 00:03:22 -0400

    Simplify xmonad config

--- xmonad.hs
+++ xmonad.hs
@@ -15,22 +15,13 @@ import XMonad.Util.CustomKeys
 import XMonad.Util.NamedWindows
 import XMonad.Util.Run
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Prompt
+import XMonad.Prompt.Shell
 
 import qualified XMonad.StackSet as W
 
 
-myScreensaver = "/usr/bin/gnome-screensaver-command --lock"
-myScreenshot = "scrot '%F-%T_$wx$h.png' -e 'mv $f ~/Pictures/Screenshots'"
-myFocusedScreenshot = myScreenshot ++ " -u"
-mySelectionScreenshot = myScreenshot ++ " -s"
-myLauncher = "$(yeganesh -x -- -fn '-*-terminus-*-r-normal-*-*-120-*-*-*-*-iso8859-*')"
 myMediaControl = "media-control "
-myNotify = "notifier "
-myBrowser = "firefox"
-myPrivateBrowser = "firefox --private-window"
-clipboardManager = "clipmenu"
-toggleInput = "toggle-macbook-trackpad"
-powerButton = "/etc/acpi/powerbtn.sh"
 
 delkeys :: XConfig l -> [(KeyMask, KeySym)]
 delkeys XConfig {XMonad.modMask = modMask} =
@@ -38,16 +29,12 @@ delkeys XConfig {XMonad.modMask = modMask} =
 
 inskeys :: XConfig l -> [((KeyMask, KeySym), X ())]
 inskeys conf@(XConfig {XMonad.modMask = modMask}) =
-  [ ((modMask .|. controlMask, xK_l), spawn (myNotify ++ "Locking;" ++ myScreensaver))
-  , ((modMask, xK_p), spawn myLauncher)
-  , ((modMask, xK_s), spawn (myNotify ++ "'Taking selection screenshot';" ++ mySelectionScreenshot))
-  , ((modMask .|. controlMask, xK_s), spawn (myNotify ++ "'Taking focused screenshot';" ++ myFocusedScreenshot))
-  , ((modMask .|. shiftMask, xK_s), spawn (myNotify ++ "'Taking screenshot';" ++ myScreenshot))
-  , ((modMask, xK_backslash), spawn clipboardManager)
-  , ((modMask, xK_b), spawn myBrowser)
-  , ((modMask .|. shiftMask, xK_b), spawn myPrivateBrowser)
-  , ((modMask, xK_i), spawn toggleInput)
-  , ((0, xF86XK_PowerOff), spawn powerButton)
+  [ ((modMask .|. controlMask, xK_l), spawn ("notifier Locking; xscreensaver-command -lock"))
+  , ((modMask, xK_p), shellPrompt def)
+  , ((modMask, xK_backslash), spawn "clipmenu")
+  , ((modMask, xK_b), spawn "firefox")
+  , ((modMask .|. shiftMask, xK_b), spawn "firefox --private-window")
+  , ((0, xF86XK_PowerOff), spawn "/etc/acpi/powerbtn.sh")
   , ((0, xF86XK_AudioPrev), spawn (myMediaControl ++ "previous"))
   , ((0, xF86XK_AudioPlay), spawn (myMediaControl ++ "play-pause"))
   , ((0, xF86XK_AudioNext), spawn (myMediaControl ++ "next"))
@@ -58,32 +45,31 @@ inskeys conf@(XConfig {XMonad.modMask = modMask}) =
   , ((shiftMask, xF86XK_AudioRaiseVolume), spawn (myMediaControl ++ "+"))
   ]
   ++
-  -- These have to be added back because they go away for some reason
   [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
     | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
-myLayout = smartBorders $ avoidStruts (
-        Tall 1 (3/100) (1/2)
-        ||| tabbed shrinkText defaultTheme { activeColor = "#000000" }
-    ) ||| noBorders (fullscreenFull Full)
+layout = tiled ||| full
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled = Tall nmaster ratio delta
 
-myManageHook = manageDocks <+> composeAll
-        [ className =? "Dialog" --> doFloat
-        , className =? "Gimp" --> doFloat
-        , className =? "Xmessage" --> doFloat
-        , title =? "Password Required" --> doFloat
-        , resource =? "desktop_window" --> doIgnore
-        , isFullscreen --> doFullFloat ]
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta = 3/100
+
+     -- Clean fullscreen
+     full = noBorders $ fullscreenFull Full
 
 main = do
-  xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
   xmonad $ defaultConfig
-    { focusFollowsMouse = False
-    , keys = customKeys delkeys inskeys
-    , layoutHook = myLayout
-    , logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn xmproc }
-    , manageHook = myManageHook
+    { keys = customKeys delkeys inskeys
+    , layoutHook = layout
     , modMask = mod4Mask
     , terminal = "/usr/bin/terminology"
     }
