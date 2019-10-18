42cd0d217ff0a11c5af42c55c1409932f1c5a175
Schlueter <b@schlueter.blue>
2019-10-18 15:25:47 -0700

    Add MonBrightness key handling

--- xmonad.hs
+++ xmonad.hs
@@ -2,16 +2,18 @@
 --
 -- Requires yegonesh, clipmenu, and services from my bin repository
 
-import Graphics.X11.ExtraTypes.XF86 ( xF86XK_AudioPrev
-                                    , xF86XK_AudioPlay
-                                    , xF86XK_AudioNext
+import Graphics.X11.ExtraTypes.XF86 ( xF86XK_AudioLowerVolume
                                     , xF86XK_AudioMute
-                                    , xF86XK_AudioLowerVolume
+                                    , xF86XK_AudioNext
+                                    , xF86XK_AudioPlay
+                                    , xF86XK_AudioPrev
                                     , xF86XK_AudioRaiseVolume
+                                    , xF86XK_MonBrightnessDown
+                                    , xF86XK_MonBrightnessUp
                                     )
 
 import XMonad
-import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.DynamicLog (ppCurrent, statusBar, xmobarPP, xmobarColor, wrap)
 import XMonad.Hooks.ManageDocks (avoidStruts)
 import XMonad.Layout.Fullscreen (fullscreenFull)
 import XMonad.Layout.NoBorders (noBorders, smartBorders)
@@ -27,7 +29,9 @@ myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "«" "»" }
 toggleStrutsKey XConfig {modMask = modMask} = (modMask, xK_h)
 
 myConfig = def { keys = customKeys delkeys inskeys
-               , layoutHook = smartBorders $ avoidStruts ( Tall 1 (3/100) (1/2)) ||| noBorders (fullscreenFull Full)
+               , layoutHook = smartBorders $ avoidStruts
+                     (Tall 1 (3/100) (1/2))
+                 ||| noBorders (fullscreenFull Full)
                , modMask = mod4Mask
                , terminal = "st tmux attach || st tmux new"
                }
@@ -39,14 +43,16 @@ inskeys conf@XConfig {modMask = modMask} =
   , ((modMask,               xK_backslash), spawn "clipmenu")
   , ((modMask,               xK_b        ), spawn "firefox")
   , ((modMask,               xK_l        ), spawn "slock")
-  , ((modMask .|. shiftMask, xK_h        ), sendMessage Shrink) -- %! Shrink the master area
-  , ((modMask .|. shiftMask, xK_l        ), sendMessage Expand) -- %! Expand the master area
+  , ((modMask .|. shiftMask, xK_h        ), sendMessage Shrink) -- Shrink the master area
+  , ((modMask .|. shiftMask, xK_l        ), sendMessage Expand) -- Expand the master area
   , ((0,         xF86XK_AudioPrev        ), spawn "mediac previous")
   , ((0,         xF86XK_AudioPlay        ), spawn "mediac play-pause")
   , ((0,         xF86XK_AudioNext        ), spawn "mediac next")
   , ((0,         xF86XK_AudioMute        ), spawn "mediac toggle-mute")
   , ((0,         xF86XK_AudioLowerVolume ), spawn "mediac -1")
   , ((0,         xF86XK_AudioRaiseVolume ), spawn "mediac +1")
+  , ((0,         xF86XK_MonBrightnessUp  ), spawn "brightness +15")
+  , ((0,         xF86XK_MonBrightnessDown), spawn "brightness -15")
   , ((shiftMask, xF86XK_AudioLowerVolume ), spawn "mediac -")
   , ((shiftMask, xF86XK_AudioRaiseVolume ), spawn "mediac +")
   ]
