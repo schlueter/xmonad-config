609b2e3f35c80907ddf8d30389efd16a67ab76ca
Schlueter <b@schlueter.blue>
2019-10-23 13:01:20 -0700

    Add wireless to xmobar

--- xmobar.hs
+++ xmobar.hs
@@ -3,11 +3,12 @@
 Config { font = "-*-terminal-medium-*-*-*-14-*-*-*-*-*-*-*"
        , position = Top
        , allDesktops = True
-       , template = "%StdinReader% }{ %kbd%  %battery%   %date%"
+       , template = "%StdinReader% }{ %wi% %kbd%  %battery%   %date%"
        , commands = [
            Run StdinReader
          , Run Kbd [("us(dvorak)", "DV"), ("us", "US")]
          , Run BatteryP ["BAT0"] ["-t", "<left>% <acstatus>", "-H", "80", "-p", "3", "--", "-i", "(On)","-O", "(On)", "-o", "(<timeleft>)", "-l", "red", "-m", "blue", "-h", "green"] 60
          , Run Date "%F %a %T" "date" 10
+         , Run Wireless "wlp3s0" ["-t", "<essid> <quality"] 60
        ]
 }
