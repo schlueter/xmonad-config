ca61e7b1202a451282639ffaf01724d513d0aa48
Schlueter <b@schlueter.blue>
2019-10-23 16:25:41 -0700

    Add MonBrightness control

--- xmonad.hs
+++ xmonad.hs
@@ -5,7 +5,9 @@ import Graphics.X11.ExtraTypes.XF86 ( xF86XK_AudioPrev
                                     , xF86XK_AudioNext
                                     , xF86XK_AudioMute
                                     , xF86XK_AudioLowerVolume
-                                    , xF86XK_AudioRaiseVolume)
+                                    , xF86XK_AudioRaiseVolume
+                                    , xF86XK_MonBrightnessDown
+                                    , xF86XK_MonBrightnessUp)
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks (avoidStruts)
@@ -37,6 +39,8 @@ inskeys conf@XConfig {modMask = modMask} =
   , ((modMask,               xK_l        ), spawn "slock")
   , ((modMask .|. shiftMask, xK_h        ), sendMessage Shrink) -- %! Shrink the master area
   , ((modMask .|. shiftMask, xK_l        ), sendMessage Expand) -- %! Expand the master area
+  , ((0,         xF86XK_MonBrightnessUp  ), spawn "brightness +15")
+  , ((0,         xF86XK_MonBrightnessDown), spawn "brightness -15")
   , ((0,         xF86XK_AudioPrev        ), spawn "mediac previous")
   , ((0,         xF86XK_AudioPlay        ), spawn "mediac play-pause")
   , ((0,         xF86XK_AudioNext        ), spawn "mediac next")
