53ccd95238e31a9ad21fb832a5b0dff0cc143fd5
Schlueter <b@schlueter.blue>
2019-11-11 13:19:07 -0800

    Update xmobar config

--- xmobar.hs
+++ xmobar.hs
@@ -1,13 +1,13 @@
 --  Author: Brandon Schlueter
 
-Config { font = "-*-terminal-medium-*-*-*-14-*-*-*-*-*-*-*"
-       , position = Top
+Config { font = "-*-terminal-medium-*-*-*-18-*-*-*-*-*-*-*"
+       , position = Bottom
        , allDesktops = True
        , template = "%StdinReader% }{ %kbd%  %battery%   %date%"
        , commands = [
            Run StdinReader
          , Run Kbd [("us(dvorak)", "DV"), ("us", "US")]
-         , Run BatteryP ["BAT0"] ["-t", "<left>% <acstatus>", "-H", "80", "-p", "3", "--", "-i", "(On)","-O", "(On)", "-o", "(<timeleft>)", "-l", "red", "-m", "blue", "-h", "green"] 60
-         , Run Date "%F %a %T" "date" 10
+         , Run BatteryP ["BAT0"] ["-t", "<left>% <acstatus>", "-H", "80", "-p", "3", "--", "-i", "(On)","-O", "(On)", "-o", "(<timeleft>)", "-l", "red", "-m", "blue", "-h", "green"] 100
+         , Run Date "%F %a %H:%M" "date" 100
        ]
 }
