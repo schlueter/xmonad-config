07189ed7a1aa22eee548c1b283a634696f509b61
Schlueter <b@schlueter.blue>
2020-04-25 23:53:26 -0700

    Apply brittany to xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -1,46 +1,53 @@
 -- Author: Brandon Schlueter
 
-import Control.Monad (join)
+import           Control.Monad                  ( join )
 
-import XMonad
-import XMonad.Actions.GridSelect
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
-import XMonad.Layout.Fullscreen (fullscreenFull)
-import XMonad.Layout.NoBorders (noBorders, smartBorders)
-import XMonad.Util.CustomKeys (customKeys)
-import XMonad.Util.EZConfig (additionalKeysP, checkKeymap)
-import XMonad.Util.NamedWindows (getName)
-import XMonad.Util.Run (safeSpawn)
-import XMonad.Layout.Circle
-import XMonad.Actions.SimpleDate
+import           XMonad
+import           XMonad.Actions.GridSelect
+import           XMonad.Hooks.DynamicLog
+import           XMonad.Hooks.ManageDocks
+import           XMonad.Hooks.ManageHelpers     ( isFullscreen
+                                                , doFullFloat
+                                                )
+import           XMonad.Layout.Fullscreen       ( fullscreenFull )
+import           XMonad.Layout.NoBorders        ( noBorders
+                                                , smartBorders
+                                                )
+import           XMonad.Util.CustomKeys         ( customKeys )
+import           XMonad.Util.EZConfig           ( additionalKeysP
+                                                , checkKeymap
+                                                )
+import           XMonad.Util.NamedWindows       ( getName )
+import           XMonad.Util.Run                ( safeSpawn )
+import           XMonad.Layout.Circle
+import           XMonad.Actions.SimpleDate
 
-import qualified XMonad.StackSet as W
+import qualified XMonad.StackSet               as W
 
 
 -- Launch polybar, obviously
 -- Run xmonad, making it aware of docks (like polybar), and providing our config
 main :: IO ()
 main = do
-    spawn "launch-polybar.sh"
-    xmonad $ docks $ additionalKeysP myConfig myKeymap
+  spawn "launch-polybar.sh"
+  xmonad $ docks $ additionalKeysP myConfig myKeymap
 
 -- Check for Nothing
 isNothing :: Maybe a -> Bool
 isNothing Nothing = True
-isNothing _ = False
+isNothing _       = False
 
-twoColumnLayout = smartBorders $ avoidStruts ( Tall 1 (3/100) (1/2))
+twoColumnLayout = smartBorders $ avoidStruts (Tall 1 (3 / 100) (1 / 2))
 oneWindowLayout = avoidStruts (noBorders (fullscreenFull Full))
 
-myConfig = def { layoutHook = twoColumnLayout ||| oneWindowLayout ||| Circle
-               , logHook = polybarHook
-               , manageHook = manageHook def <+> manageDocks
-               , modMask = mod4Mask -- ⌘  key on mac
-               , terminal = "kitty"
+myConfig = def { layoutHook  = twoColumnLayout ||| oneWindowLayout ||| Circle
+               , logHook     = polybarHook
+               , manageHook  = manageHook def <+> manageDocks
+               , modMask     = mod4Mask -- ⌘  key on mac
+               , terminal    = "kitty"
                -- Enable keyboard shortcut configuration via simple aliases
-               , startupHook = return () >> checkKeymap myConfig myKeymap }
+               , startupHook = return () >> checkKeymap myConfig myKeymap
+               }
 
 -- Formatting for workspace display in polybar
 currentFormat :: String -> String
@@ -52,30 +59,37 @@ notInUseFormat tag = " " ++ tag ++ " "
 format :: String -> W.Workspace String l a -> String
 format current_workspace workspace
   | current_workspace == W.tag workspace = currentFormat $ W.tag workspace
-  | isNothing (W.stack workspace) = inUseFormat $ W.tag workspace
-  | otherwise = notInUseFormat $ W.tag workspace
+  | isNothing (W.stack workspace)        = inUseFormat $ W.tag workspace
+  | otherwise                            = notInUseFormat $ W.tag workspace
 
 -- Populate fifos for polybar
 polybarHook = do
   winset <- gets windowset
-  title <- maybe (return "") (fmap show . getName) . W.peek $ winset
+  title  <- maybe (return "") (fmap show . getName) . W.peek $ winset
   let current_workspace = W.currentTag winset
-  let workspaces_string = join $ map (format current_workspace) $ W.workspaces winset
+  let workspaces_string =
+        join $ map (format current_workspace) $ W.workspaces winset
 
   io $ appendFile "/tmp/.xmonad-title-log" (title ++ "\n")
   io $ appendFile "/tmp/.xmonad-workspace-log" (workspaces_string ++ "\n")
 
 myKeymap =
-  [ ("M-M1-<Space>"            , spawn "cycle-keyboard-layout dvorak us") -- mod + alt + space
-  , ("M-p"                     , spawn "yegonesh")
-  , ("M-\\"                    , spawn "clipmenu")
-  , ("M-b"                     , spawn "firefox")
-  , ("M-l"                     , spawn "slock")
-  , ("M-d"                     , date)
-  , ("M-g"                     , goToSelected def)
-  , ("M-h"                     , sendMessage ToggleStruts)
-  , ("M-S-h"                   , sendMessage Shrink) -- %! Shrink the master area
-  , ("M-S-l"                   , sendMessage Expand) -- %! Expand the master area
+  [ ( "M-M1-<Space>"
+    , spawn "cycle-keyboard-layout dvorak us"
+    ) -- mod + alt + space
+  , ("M-p" , spawn "yegonesh")
+  , ("M-\\", spawn "clipmenu")
+  , ("M-b" , spawn "firefox")
+  , ("M-l" , spawn "slock")
+  , ("M-d" , date)
+  , ("M-g" , goToSelected def)
+  , ("M-h" , sendMessage ToggleStruts)
+  , ( "M-S-h"
+    , sendMessage Shrink
+    ) -- %! Shrink the master area
+  , ( "M-S-l"
+    , sendMessage Expand
+    ) -- %! Expand the master area
   , ("<XF86MonBrightnessUp>"   , spawn "brightness +15")
   , ("<XF86MonBrightnessDown>" , spawn "brightness -15")
   , ("<XF86AudioPrev>"         , spawn "mediac previous")
@@ -85,4 +99,5 @@ myKeymap =
   , ("<XF86AudioLowerVolume>"  , spawn "mediac -1")
   , ("<XF86AudioRaiseVolume>"  , spawn "mediac +1")
   , ("S-<XF86AudioLowerVolume>", spawn "mediac -")
-  , ("S-<XF86AudioRaiseVolume>", spawn "mediac +") ]
+  , ("S-<XF86AudioRaiseVolume>", spawn "mediac +")
+  ]
