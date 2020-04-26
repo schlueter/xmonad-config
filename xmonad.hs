f22611373eda8f485eed29ea98a57826c62cbd55
Schlueter <b@schlueter.blue>
2020-04-26 09:41:55 -0700

    Add types clean up imports

--- xmonad.hs
+++ xmonad.hs
@@ -3,24 +3,22 @@
 import           Control.Monad                  ( join )
 
 import           XMonad
-import           XMonad.Actions.GridSelect
-import           XMonad.Hooks.DynamicLog
+import           XMonad.ManageHook              ( title )
 import           XMonad.Hooks.ManageDocks
-import           XMonad.Hooks.ManageHelpers     ( isFullscreen
-                                                , doFullFloat
-                                                )
 import           XMonad.Layout.Fullscreen       ( fullscreenFull )
 import           XMonad.Layout.NoBorders        ( noBorders
                                                 , smartBorders
                                                 )
-import           XMonad.Util.CustomKeys         ( customKeys )
 import           XMonad.Util.EZConfig           ( additionalKeysP
                                                 , checkKeymap
                                                 )
 import           XMonad.Util.NamedWindows       ( getName )
-import           XMonad.Util.Run                ( safeSpawn )
 import           XMonad.Layout.Circle
 import           XMonad.Actions.SimpleDate
+import           XMonad.Layout.LayoutModifier   ( ModifiedLayout )
+import           XMonad.Layout.NoBorders        ( SmartBorder )
+import           XMonad.Layout.NoBorders        ( WithBorder )
+import           XMonad.Layout.Fullscreen       ( FullscreenFull )
 
 import qualified XMonad.StackSet               as W
 
@@ -28,25 +26,59 @@ import qualified XMonad.StackSet               as W
 -- Launch polybar, obviously
 -- Run xmonad, making it aware of docks (like polybar), and providing our config
 main :: IO ()
-main = do
+main = xmonad $ docks $ additionalKeysP myConfig myKeymap
+
+myStartUpHook :: X ()
+myStartUpHook = do
+  return ()
+  checkKeymap myConfig myKeymap
   spawn "launch-polybar.sh"
-  xmonad $ docks $ additionalKeysP myConfig myKeymap
+
 
 -- Check for Nothing
 isNothing :: Maybe a -> Bool
 isNothing Nothing = True
 isNothing _       = False
 
+twoColumnLayout
+  :: ModifiedLayout SmartBorder (ModifiedLayout AvoidStruts Tall) a
 twoColumnLayout = smartBorders $ avoidStruts (Tall 1 (3 / 100) (1 / 2))
+
+oneWindowLayout
+  :: ModifiedLayout
+       AvoidStruts
+       (ModifiedLayout WithBorder (ModifiedLayout FullscreenFull Full))
+       Window
 oneWindowLayout = avoidStruts (noBorders (fullscreenFull Full))
 
+myConfig
+  :: XConfig
+       ( Choose
+           ( XMonad.Layout.LayoutModifier.ModifiedLayout
+               XMonad.Layout.NoBorders.SmartBorder
+               (XMonad.Layout.LayoutModifier.ModifiedLayout AvoidStruts Tall)
+           )
+           ( Choose
+               ( XMonad.Layout.LayoutModifier.ModifiedLayout
+                   AvoidStruts
+                   ( XMonad.Layout.LayoutModifier.ModifiedLayout
+                       XMonad.Layout.NoBorders.WithBorder
+                       ( XMonad.Layout.LayoutModifier.ModifiedLayout
+                           XMonad.Layout.Fullscreen.FullscreenFull
+                           Full
+                       )
+                   )
+               )
+               Circle
+           )
+       )
 myConfig = def { layoutHook  = twoColumnLayout ||| oneWindowLayout ||| Circle
                , logHook     = polybarHook
                , manageHook  = manageHook def <+> manageDocks
                , modMask     = mod4Mask -- ⌘  key on mac
                , terminal    = "kitty"
                -- Enable keyboard shortcut configuration via simple aliases
-               , startupHook = return () >> checkKeymap myConfig myKeymap
+               , startupHook = myStartUpHook
                }
 
 -- Formatting for workspace display in polybar
@@ -56,6 +88,7 @@ inUseFormat :: String -> String
 inUseFormat tag = " %{F#666}" ++ tag ++ " %{B- F-}"
 notInUseFormat :: String -> String
 notInUseFormat tag = " " ++ tag ++ " "
+
 format :: String -> W.Workspace String l a -> String
 format current_workspace workspace
   | current_workspace == W.tag workspace = currentFormat $ W.tag workspace
@@ -63,6 +96,7 @@ format current_workspace workspace
   | otherwise                            = notInUseFormat $ W.tag workspace
 
 -- Populate fifos for polybar
+polybarHook :: X ()
 polybarHook = do
   winset <- gets windowset
   title  <- maybe (return "") (fmap show . getName) . W.peek $ winset
@@ -73,6 +107,7 @@ polybarHook = do
   io $ appendFile "/tmp/.xmonad-title-log" (title ++ "\n")
   io $ appendFile "/tmp/.xmonad-workspace-log" (workspaces_string ++ "\n")
 
+myKeymap :: [(String, X ())]
 myKeymap =
   [ ( "M-M1-<Space>"
     , spawn "cycle-keyboard-layout dvorak us"
@@ -82,7 +117,6 @@ myKeymap =
   , ("M-b" , spawn "firefox")
   , ("M-l" , spawn "slock")
   , ("M-d" , date)
-  , ("M-g" , goToSelected def)
   , ("M-h" , sendMessage ToggleStruts)
   , ( "M-S-h"
     , sendMessage Shrink
